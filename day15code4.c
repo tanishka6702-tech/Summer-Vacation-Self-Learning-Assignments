ce77d0ac17e0c488e720aa7c9383163abe2ef4d2
tanishka6702-tech <tanishka6702@gmail.com>
2026-06-20 14:53:20 +0530

    Add files via upload

---
+++ day15code4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j = 0;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i = 0; i < n; i++) {
+        if(arr[i] != 0) {
+            arr[j] = arr[i];
+            j++;
+        }
+    }
+
+    while(j < n) {
+        arr[j] = 0;
+        j++;
+    }
+
+    printf("Array after moving zeros to end:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
