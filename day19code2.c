db1aa17dd818400315e2a0ff0255afd714393c87
tanishka6702-tech <tanishka6702@gmail.com>
2026-06-22 21:08:24 +0530

    Add files via upload

---
+++ day19code2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], b[10][10], diff[10][10];
+    int r, c, i, j;
+
+    printf("Enter rows and columns: ");
+    scanf("%d%d", &r, &c);
+
+    printf("Enter first matrix:\n");
+    for(i = 0; i < r; i++)
+        for(j = 0; j < c; j++)
+            scanf("%d", &a[i][j]);
+
+    printf("Enter second matrix:\n");
+    for(i = 0; i < r; i++)
+        for(j = 0; j < c; j++)
+            scanf("%d", &b[i][j]);
+
+    for(i = 0; i < r; i++)
+        for(j = 0; j < c; j++)
+            diff[i][j] = a[i][j] - b[i][j];
+
+    printf("Difference Matrix:\n");
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++)
+            printf("%d ", diff[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
