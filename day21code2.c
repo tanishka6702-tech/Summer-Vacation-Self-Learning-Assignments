25b3d6df63bdf23ba0a6842417de182ecabe9864
tanishka6702-tech <tanishka6702@gmail.com>
2026-06-25 21:02:56 +0530

    Add files via upload

---
+++ day21code2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int len = 0, i;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    while (str[len] != '\0') {
+        len++;
+    }
+
+    printf("Reversed string: ");
+
+    for (i = len - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
